d36c30fd6679578312336f0635dacfe7571c5393
Lekanich <lekan1992@gmail.com>
2018-05-18 11:56:34 +0300

    - add splitter - serialization round

--- src/main/java/com/wixpress/test/tree/BinTreeSerialization.java
+++ src/main/java/com/wixpress/test/tree/BinTreeSerialization.java
@@ -9,6 +9,7 @@ public class BinTreeSerialization {
     private static final String NULL_CONSTANT = "null";
     public static final String OPEN_SYMBOL = "[";
     public static final String CLOSE_SYMBOL = "]";
+    public static final String SPLITTER = ",";
 
     public static String serialize(BinTree bintree) throws BinTreeSerializationException {
         if (bintree == null) return null;
@@ -18,45 +19,58 @@ public class BinTreeSerialization {
         Set<BinTree> serializedNodes = new HashSet<>();
         Queue<BinTree> queue = new LinkedList<>();
         queue.offer(bintree);
-
         Queue<BinTree> nextParents = new LinkedList<>();
-        for (BinTree parent : queue) {
-            // serialize
-            appendValue(parent, builder);
 
-            // if current parent == null so he doesn't have children
-            if (parent == null) {
-                continue;
+        // payload
+        while (!queue.isEmpty()) {
+
+            // round
+            for (BinTree parent : queue) {
+                // serialize
+                appendValue(parent, builder);
+
+                // if current parent == null so he doesn't have children
+                if (parent == null) {
+                    continue;
+                }
+
+                // add to already serialized list
+                serializedNodes.add(parent);
+
+                // save as parent left child
+                if (serializedNodes.contains(parent.getLeft())) {
+                    throw new BinTreeSerializationException("Left node was already serialized");
+                } else {
+                    nextParents.offer(parent.getLeft());
+                }
+
+                // save as parent right child
+                if (serializedNodes.contains(parent.getRight())) {
+                    throw new BinTreeSerializationException("Right node was already serialized");
+                } else {
+                    nextParents.offer(parent.getRight());
+                }
             }
 
-            // add to already serialized list
-            serializedNodes.add(parent);
-
-            // save as parent left child
-            if (serializedNodes.contains(parent.getLeft())) {
-                throw new BinTreeSerializationException("Left node was already serialized");
-            } else {
-                nextParents.offer(parent.getLeft());
-            }
-
-            // save as parent right child
-            if (serializedNodes.contains(parent.getRight())) {
-                throw new BinTreeSerializationException("Right node was already serialized");
-            } else {
-                nextParents.offer(parent.getRight());
-            }
+            // prepare next round
+            queue = nextParents;
+            nextParents = new LinkedList<>();
         }
 
-
-        throw new BinTreeSerializationException();
+        // return
+        return builder.toString();
     }
 
-    private static StringBuilder appendValue(BinTree node, StringBuilder builder) {
+    private static void appendValue(BinTree node, StringBuilder builder) {
         if (node == null) {
-            return builder.append(NULL_CONSTANT);
+            builder.append(NULL_CONSTANT);
+        } else {
+            builder.append(OPEN_SYMBOL)
+                    .append(node.getValue())
+                    .append(CLOSE_SYMBOL);
         }
 
-        return builder.append(OPEN_SYMBOL).append(node.getValue()).append(CLOSE_SYMBOL);
+        builder.append(SPLITTER);
     }
 }
 
